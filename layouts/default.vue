e1dcd5147ad1f9dd792a010f6463726d86240ad7
yukimaruDubstep <k.yuuki1997@gmail.com>
2021-11-27 10:36:20 +0900

    :tada: create nuxt2 script setup exsample

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <CThemeProvider>
+      <CColorModeProvider>
+        <CBox font-family="body" as="main">
+          <CReset />
+          <Nuxt />
+        </CBox>
+      </CColorModeProvider>
+    </CThemeProvider>
+  </div>
+</template>
+<script setup lang="ts">
+import {
+  CThemeProvider,
+  CColorModeProvider,
+  CReset,
+  CBox
+} from '@chakra-ui/vue'
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <TutorialVue :label="text"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from '@nuxtjs/composition-api';
+import TutorialVue from '~/components/Tutorial.vue';
+const text = ref<string>("123456789");
+</script>
